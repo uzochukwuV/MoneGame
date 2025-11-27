648d4662d317c7dc56ef05b4f74600f16330fe01
uzochukwuv <vic.ezealor@gmail.com>
2025-11-27 22:42:49 +0100

    added amnswers v2

--- client/src/config/game.ts
+++ client/src/config/game.ts
@@ -1,5 +1,5 @@
 // Replace with your deployed package ID
-export const GAME_PACKAGE_ID = '0x16d2cab2772b1fc4372cefe3a50c76bc3c18feb9b7b685f56cd7b46c9e923d0a';
+export const GAME_PACKAGE_ID = '0xfe41a2c00448b8aa8f99bbc19d98dea558ce2df7d9fd390cf1eda76354d2db2f';
 // real = 0xfe41a2c00448b8aa8f99bbc19d98dea558ce2df7d9fd390cf1eda76354d2db2f
 // Tier entry fees in MIST (1 SUI = 1_000_000_000 MIST)
 export const TIER_ENTRY_FEES = {
